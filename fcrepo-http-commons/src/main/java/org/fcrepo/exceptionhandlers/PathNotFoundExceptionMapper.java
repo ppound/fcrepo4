16eb6ad310d74bbf720a25310df8587b7189a950
fasseg <frank.asseg@googlemail.com>
2013-03-20 21:48:47 +0100

    using commons-exception handlers in resteasy, annotations added

--- fcrepo-http-commons/src/main/java/org/fcrepo/exceptionhandlers/PathNotFoundExceptionMapper.java
+++ fcrepo-http-commons/src/main/java/org/fcrepo/exceptionhandlers/PathNotFoundExceptionMapper.java
@@ -4,10 +4,14 @@ package org.fcrepo.exceptionhandlers;
 import static javax.ws.rs.core.Response.status;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
+import javax.inject.Named;
 import javax.jcr.PathNotFoundException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
+@Named
+@Provider
 public class PathNotFoundExceptionMapper implements
         ExceptionMapper<PathNotFoundException> {
 
--- fcrepo-http-commons/src/main/java/org/fcrepo/exceptionhandlers/WildcardExceptionMapper.java
+++ fcrepo-http-commons/src/main/java/org/fcrepo/exceptionhandlers/WildcardExceptionMapper.java
@@ -4,9 +4,13 @@ package org.fcrepo.exceptionhandlers;
 import static com.google.common.base.Throwables.getStackTraceAsString;
 import static javax.ws.rs.core.Response.serverError;
 
+import javax.inject.Named;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
+@Named
+@Provider
 public class WildcardExceptionMapper implements ExceptionMapper<Exception> {
 
     Boolean showStackTrace;
